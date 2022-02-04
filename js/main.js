c5398a329200a8f66702ec47dea0b7355f0477b4
laziza@mail.ru <laziza00>
2022-02-04 08:41:52 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let a = 0;
+
+document.getElementById('darkbtn').addEventListener('click', function () 
+{document.body.classList.toggle('dark');
+if (a==0){
+    this.textContent =('');
+    a++;
+}else {
+    this.textContent =('');
+    a=0;
+}
+});
+
+
+
+
+
+
+
+  
+let elmenu__burger = document.getElementById('menuBurger');
+let elheadermenu = document.getElementById('headerMenu');
+let elIcon = document.getElementById('icon');
+
+let count = 0;
+elmenu__burger.addEventListener('click', function () {
+  elheadermenu.classList.toggle('show');
+  if (count == 0) {
+    elmenu__burger.style.color = '#000';
+    elIcon.className = "bx bx-x";
+    count++;
+  } else {
+    elmenu__burger.style.color = '#000';
+    elIcon.className = "bx bx-menu";
+    count = 0;
+  }
+})
